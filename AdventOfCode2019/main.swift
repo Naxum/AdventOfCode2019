122512518d2390e57b44842badd5e7c898e3b1d1
Jake Sawyer <jake@naxite.com>
2019-12-01 19:58:46 -0800

    Initial Commit

---
+++ AdventOfCode2019/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdventOfCode2019
+//
+//  Created by Jake Sawyer on 12/1/19.
+//  Copyright © 2019 Jake Sawyer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
