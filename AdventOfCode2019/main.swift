2631b2a5aaa412b485de2de3dd6af96e3d83240a
Jake Sawyer <jake@naxite.com>
2019-12-05 21:54:09 -0800

    Day 6
    Will do Day 5 later

--- AdventOfCode2019/main.swift
+++ AdventOfCode2019/main.swift
@@ -2,10 +2,4 @@ import Foundation
 
 print("Hello, World!")
 
-print("-----")
-print("Solution 1:")
-print(Day4().solution1())
-
-print("-----")
-print("Solution 2:")
-print(Day4().solution2())
+print(Day6().solution2())
